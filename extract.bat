9c11e0af51d3d401ff265f53918ae7394676a7f1
Emmett Casey <temetvince@gmail.com>
2022-02-28 17:12:59 -0600

    Add extract.bat to extract files from x rebirth

---
+++ extract.bat
@@ -0,0 +1,5 @@
+C:
+cd "Program Files (x86)\Steam\steamapps\common\X Rebirth"
+md extracted
+XRCatTool.exe -in 01.cat -in 02.cat -in 03.cat -in 04.cat -in 05.cat -in 06.cat -in 07.cat -in 08.cat -in 09.cat -out "C:\Program Files (x86)\Steam\steamapps\common\X Rebirth\extracted"
+pause
